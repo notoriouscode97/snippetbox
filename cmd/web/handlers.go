f8ed8a875d94c3f2642566be3cccf2edcbef7a04
Dusan Dimic <dusan.dimic@vegait.rs>
2024-09-16 13:54:53 +0200

    Chapter 8.1 Setting up a HTML form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,31 +11,26 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	// Because httprouter matches the "/" path exactly, we can now remove the // manual check of r.URL.Path != "/" from this handler.
 	snippets, err := app.snippets.Latest()
-
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
-
 	app.render(w, http.StatusOK, "home.tmpl", data)
 }
-
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	// When httprouter is parsing a request, the values of any named parameters // will be stored in the request context. We'll talk about request context // in detail later in the book, but for now it's enough to know that you can // use the ParamsFromContext() function to retrieve a slice containing these // parameter names and values like so:
 	params := httprouter.ParamsFromContext(r.Context())
-
+	// We can then use the ByName() method to get the value of the "id" named // parameter from the slice and validate it as normal.
 	id, err := strconv.Atoi(params.ByName("id"))
-
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
-
 	snippet, err := app.snippets.Get(id)
-
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
 			app.notFound(w)
@@ -44,15 +39,14 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
-
 	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -61,11 +55,11 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	expires := 7
 
 	id, err := app.snippets.Insert(title, content, expires)
-
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-
+	// Update the redirect path to use the new clean URL format.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+
 }
