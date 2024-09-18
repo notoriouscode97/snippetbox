daabe7f2654062ff68f1b08af747346ef1023fb7
Dusan Dimic <dusan.dimic@vegait.rs>
2024-09-18 10:56:26 +0200

    Chapter 14.3 End-to-end testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,30 @@
 package main
 
 import (
-	"bytes"
 	"github.com/notoriouscode97/snippetbox/internal/assert"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	// Create a new instance of our application struct. For now, this just
+	// contains a couple of mock loggers (which discard anything written to
+	// them).
+	app := newTestApplication(t)
+	// We then use the httptest.NewTLSServer() function to create a new test
+	// server, passing in the value returned by our app.routes() method as the
+	// handler for the server. This starts up a HTTPS server which listens on a
+	// randomly-chosen port of your local machine for the duration of the test.
+	// Notice that we defer a call to ts.Close() so that the server is shutdown
+	// when the test finishes.
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	// The network address that the test server is listening on is contained in
+	// the ts.URL field. We can use this along with the ts.Client().Get() method
+	// to make a GET /ping request against the test server. This returns a
+	// http.Response struct containing the response.
+	code, _, body := ts.get(t, "/ping")
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ping(rr, r)
-
-	rs := rr.Result()
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-
-	body, err := io.ReadAll(rs.Body)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
